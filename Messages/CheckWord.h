d38982fe65db595d2ad7ffac62f1f8b6c1b81ce4
Tristan Schieler <tristanschieler@gmail.com>
2023-02-02 23:09:53 -0500

    convert to cmake

---
+++ Messages/CheckWord.h
@@ -0,0 +1,14 @@
+#include "Message.h"
+
+class CheckWord : public Message
+{
+    public:
+
+    void process()
+    {
+
+    }
+
+    private:
+
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "WordBoard/wordBoard.h"
-#include "Utils/FileReader.h"
+#include "wordBoard.h"
+#include "FileReader.h"
 
 int main()
 {
     FileReader boardFile;
-    std::vector<std::string> words = boardFile.read("Utils/BoardFiles/PokemonGen1.txt");
+    std::vector<std::string> words = boardFile.read("BoardFiles/PokemonGen1.txt");
 
     WordBoard game(10,10);
     game.createBoard(words);
